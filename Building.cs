04c1cb56083581ecfbf63fb9266c2930ea1a840a
bitzy06 <70433613+bitzy06@users.noreply.github.com>
2025-07-11 23:21:54 +1000

    Refactor city generation

--- Building.cs
+++ Building.cs
@@ -6,5 +6,11 @@ namespace StrategyGame
     {
         public Nts.Polygon Footprint { get; set; }
         public LandUseType LandUse { get; set; }
+
+        // Fields used by the BuildingRefiner
+        public int Level { get; set; }
+        public int PopulationCapacity { get; set; }
+        public double EconomicOutput { get; set; }
+        public double PollutionOutput { get; set; }
     }
 }
