dc02f9fc965210d689d5eff1d1cc0988a737bbea
bitzy06 <70433613+bitzy06@users.noreply.github.com>
2025-07-21 12:51:25 +1000

    Use quantized endpoints for road grouping

--- ProceduralCityRenderer.cs
+++ ProceduralCityRenderer.cs
@@ -219,63 +219,77 @@ namespace StrategyGame
 
         private static List<List<(double X, double Y)>> GroupSegments(List<LineSegment> segments)
         {
-            var result = new List<List<(double X, double Y)>>();
-            var remaining = new List<LineSegment>(segments);
+            var pointsToSegments = new Dictionary<QuantizedPoint, List<LineSegment>>(segments.Count * 2);
+            var visited = new HashSet<LineSegment>(segments.Count);
+            var polylines = new List<List<(double X, double Y)>>();
 
-            while (remaining.Count > 0)
+            void AddPoint(double x, double y, LineSegment s)
             {
-                var seg = remaining[^1];
-                remaining.RemoveAt(remaining.Count - 1);
-                var poly = new List<(double X, double Y)> { (seg.X1, seg.Y1), (seg.X2, seg.Y2) };
-
-                bool changed;
-                do
+                var qp = new QuantizedPoint(x, y);
+                if (!pointsToSegments.TryGetValue(qp, out var list))
                 {
-                    changed = false;
-                    for (int i = 0; i < remaining.Count; i++)
-                    {
-                        var s = remaining[i];
-                        if (PointsEqual(poly[^1], (s.X1, s.Y1)))
-                        {
-                            poly.Add((s.X2, s.Y2));
-                            remaining.RemoveAt(i);
-                            changed = true;
-                            break;
-                        }
-                        if (PointsEqual(poly[^1], (s.X2, s.Y2)))
-                        {
-                            poly.Add((s.X1, s.Y1));
-                            remaining.RemoveAt(i);
-                            changed = true;
-                            break;
-                        }
-                        if (PointsEqual(poly[0], (s.X2, s.Y2)))
-                        {
-                            poly.Insert(0, (s.X1, s.Y1));
-                            remaining.RemoveAt(i);
-                            changed = true;
-                            break;
-                        }
-                        if (PointsEqual(poly[0], (s.X1, s.Y1)))
-                        {
-                            poly.Insert(0, (s.X2, s.Y2));
-                            remaining.RemoveAt(i);
-                            changed = true;
-                            break;
-                        }
-                    }
-                } while (changed);
-
-                result.Add(poly);
+                    list = new List<LineSegment>();
+                    pointsToSegments[qp] = list;
+                }
+                list.Add(s);
             }
 
-            return result;
-        }
+            foreach (var s in segments)
+            {
+                AddPoint(s.X1, s.Y1, s);
+                AddPoint(s.X2, s.Y2, s);
+            }
 
-        private static bool PointsEqual((double X, double Y) a, (double X, double Y) b)
-        {
-            const double Eps = 1e-9;
-            return Math.Abs(a.X - b.X) < Eps && Math.Abs(a.Y - b.Y) < Eps;
+            foreach (var start in segments)
+            {
+                if (!visited.Add(start))
+                    continue;
+
+                var poly = new LinkedList<(double X, double Y)>();
+                poly.AddLast((start.X1, start.Y1));
+                poly.AddLast((start.X2, start.Y2));
+
+                void ExtendForward(QuantizedPoint current)
+                {
+                    while (pointsToSegments.TryGetValue(current, out var conns))
+                    {
+                        var next = conns.FirstOrDefault(s => !visited.Contains(s));
+                        if (next == null)
+                            break;
+
+                        visited.Add(next);
+                        var p1 = new QuantizedPoint(next.X1, next.Y1);
+                        current = current.Equals(p1)
+                            ? new QuantizedPoint(next.X2, next.Y2)
+                            : p1;
+                        poly.AddLast((current.X, current.Y));
+                    }
+                }
+
+                void ExtendBackward(QuantizedPoint current)
+                {
+                    while (pointsToSegments.TryGetValue(current, out var conns))
+                    {
+                        var next = conns.FirstOrDefault(s => !visited.Contains(s));
+                        if (next == null)
+                            break;
+
+                        visited.Add(next);
+                        var p1 = new QuantizedPoint(next.X1, next.Y1);
+                        current = current.Equals(p1)
+                            ? new QuantizedPoint(next.X2, next.Y2)
+                            : p1;
+                        poly.AddFirst((current.X, current.Y));
+                    }
+                }
+
+                ExtendForward(new QuantizedPoint(start.X2, start.Y2));
+                ExtendBackward(new QuantizedPoint(start.X1, start.Y1));
+
+                polylines.Add(new List<(double X, double Y)>(poly));
+            }
+
+            return polylines;
         }
 
         private static void DrawRoads(Image<Rgba32> img, Guid modelId, IEnumerable<LineSegment> roads, GeoBounds bounds, int cellSize, int tileX, int tileY)
